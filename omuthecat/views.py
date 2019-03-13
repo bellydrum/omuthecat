aa01ef71c1d8d1191d0d2134912b5d40aec1bd50
bellydrum <davidmaness1990@gmail.com>
2019-03-13 12:03:23 -0500

    BUG FIX - rename staticfiles to static in views.render_home

--- omuthecat/views.py
+++ omuthecat/views.py
@@ -7,7 +7,7 @@ from random import randint
 def render_home(request):
 
     # get random file from images/omu
-    omu_image_folder = 'staticfiles/images/omu/'
+    omu_image_folder = 'static/images/omu/'
     image_filenames = listdir(omu_image_folder)
     filename = image_filenames[ randint( 0, len(image_filenames) - 1 ) ]
 
