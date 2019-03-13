758184f6a0f1bfd6ea9133eeecf823461c5cd490
bellydrum <davidmaness1990@gmail.com>
2019-03-13 12:02:02 -0500

    change name and references of /staticfiles/ to /static/

--- omu/settings.py
+++ omu/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # automatically take care of compressing static files on deploy
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
