1f368435bc8a548ff01db825767ba629b7cdbc98
bellydrum <davidmaness1990@gmail.com>
2019-03-14 03:34:43 +0000

    BUG FIX - remove deprecated url

--- omuthecat/urls.py
+++ omuthecat/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     url('^$', render_home, name='home'),
 
     # api urls
-    url('^change_picture$', change_picture, name='change_picture'),
     url('^log_clicks$', log_clicks, name='log_clicks'),
 ]
