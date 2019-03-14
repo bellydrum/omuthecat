0547f3386a11473eb514117b2d09d35287862a96
David Maness <davidmaness@Davids-MacBook-Pro.local>
2019-03-13 21:29:48 -0500

    add ClickLog to models; add migration

--- omuthecat/models.py
+++ omuthecat/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ClickLog(models.Model):
+    number_of_clicks = models.PositiveSmallIntegerField(
+        blank=False,
+        null=False
+    )
+    datetime_logged = models.DateTimeField(
+        blank=False,
+        null=False
+    )
+    # custom save() method in lieu of .auto_now()
+    # https://stackoverflow.com/a/1737078
+    def save(self, *args, **kwargs):
+        ''' on save, update timestamps '''
+        if not self.user_id:
+            self.datetime_logged = timezone.localtime(timezone.now())
+        return super(ClickLog, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'click_log'
