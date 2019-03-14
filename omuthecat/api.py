0c407e4fea7ffc0f4ea7fe492507f862a9d87da2
bellydrum <davidmaness1990@gmail.com>
2019-03-14 10:52:10 -0500

    add high score tracker

--- omuthecat/api.py
+++ omuthecat/api.py
@@ -3,13 +3,21 @@ from django.views.decorators.csrf import csrf_protect
 
 from .models import ClickLog
 
+
+################## called by views.py
+
+@csrf_protect
+def get_high_score(request):
+    if request.method == 'GET':
+        return max( [ int(log.number_of_clicks) for log in ClickLog.objects.all() ] )
+
+
+
+################## called by urls.py
+
 @csrf_protect
 def log_clicks(request):
     if request.method == 'POST':
-        clicks = int(request.POST['clicks'])
-
-        # store log of clicks
-        log = ClickLog(number_of_clicks=clicks)
+        log = ClickLog(number_of_clicks=int(request.POST['clicks']))
         log.save()
-
         return HttpResponse( { 'status': 'OK' } )
--- omuthecat/views.py
+++ omuthecat/views.py
@@ -5,23 +5,30 @@ from django.views.decorators.csrf import csrf_protect
 from os import listdir
 from random import randint
 
+from .api import get_high_score
+
 
 ################## page views
 
 @csrf_protect
 def render_home(request):
 
+    # initialize constants
     image_folder = 'omu/'
     filename = get_random_image(image_folder)
     filenames = listdir('static/images/{}'.format(image_folder))
 
+    # get click high score
+    high_score = get_high_score(request)
+
     context = {
         'csrftoken': get_token(request),
+        'high_score': high_score,
         'filename': filename,
         'filenames': filenames
     }
 
-    return render(request, "home.html", context)
+    return render(request, 'home.html', context)
 
 
 
