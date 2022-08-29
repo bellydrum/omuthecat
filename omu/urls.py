0c61ee60932bb82b89e33bd2404b2e2b4c9163b8
David Maness <david@inclusively.com>
2022-08-29 13:30:15 -0500

    updates

--- omu/urls.py
+++ omu/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('omuthecat.urls')),
-    url(r'', include('django.contrib.auth.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'', include('omuthecat.urls')),
+    re_path(r'', include('django.contrib.auth.urls')),
 ]
--- omuthecat/urls.py
+++ omuthecat/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from .api import post_clicks, get_guestbook_entries, submit_guestbook_entry
 from .views import render_home
 
 urlpatterns = [
     # view urls
-    url('^$', render_home, name='home'),
+    re_path('^$', render_home, name='home'),
 
     # api urls
-    url('^post_clicks$', post_clicks, name='post_clicks'),
-    url('^get_guestbook_entries$', get_guestbook_entries, name='get_guestbook_entries'),
-    url('^submit_guestbook_entry$', submit_guestbook_entry, name='submit_guestbook_entry'),
+    re_path('^post_clicks$', post_clicks, name='post_clicks'),
+    re_path('^get_guestbook_entries$', get_guestbook_entries, name='get_guestbook_entries'),
+    re_path('^submit_guestbook_entry$', submit_guestbook_entry, name='submit_guestbook_entry'),
 ]
